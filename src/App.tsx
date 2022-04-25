dec7e01530564d33d0e9ce51a677a8e3f17e933a
firsttoflower <104393250+firsttoflower@users.noreply.github.com>
2022-04-25 17:05:16 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import F2Flogo from './F2F-Logo-2-color-24.png';
 import './App.css';
 
@@ -8,7 +7,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={F2Flogo} className="App-logo" alt="F2F Logo" />
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
